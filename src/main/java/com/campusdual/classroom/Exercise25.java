4c9cf20523aaa3b2bf1665d48111229bec635780
Nataly Alvarado Lujan <nataly.al.luj@gmail.com>
2024-10-01 14:39:02 +0200

    Resuelto.

--- src/main/java/com/campusdual/classroom/Exercise25.java
+++ src/main/java/com/campusdual/classroom/Exercise25.java
@@ -5,16 +5,29 @@ import java.util.Stack;
 public class Exercise25 {
     public static Stack<String> createStack() {
 
+        Stack<String> pila = new Stack<>();
+        pila.add("Smith");
+        pila.add("Montessori");
+        pila.add("Peralta");
+        pila.add("House");
+
+        return pila;
     }
 
     public static void printAndEmptyStack(Stack<String> stack) {
+        int tamanoPila = stack.size();
 
+        for (int i = 0; i < tamanoPila; i++) {
+            System.out.println(stack.pop());
+        }
     }
 
     public static void main(String[] args) {
 
+        Stack<String> pila = createStack();
+        printAndEmptyStack(pila);
+        printAndEmptyStack(pila); // vacía!
     }
-
 
 
 }
